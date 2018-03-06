26e9af302ce54a4fc88d16e7226e842327a8a09c
1ilsang <1ilsang@naver.com>
2018-03-06 21:04:25 +0900

    7장

--- mytestcode.js
+++ mytestcode.js
@@ -1,18 +1,11 @@
-const bruce = { name: "1ilsang" };
-const madeline = { name: "Madeline" };
-
-function update(birthYear, occupation) {
-    this.birthYear = birthYear;
-    this.occupation = occupation;
+let f; //정의되지 않았다.
+{
+    let o = { note: 'Safe' };
+    f = function() {
+        return o;
+    };
 }
-console.log(bruce);
-const updateBruce = update.bind(bruce);
-updateBruce("1994", "code");
-console.log(bruce);
-updateBruce.call(madeline, 2018, "sangchul");
-console.log(bruce);
-
-const updateMadel = update.bind(madeline);
-updateMadel("2222", "222");
-console.log(madeline);
-console.log(updateMadel);
+let oRef = f();
+console.log(oRef);
+oRef.note = "Not so safe after all!";
+console.log(oRef);

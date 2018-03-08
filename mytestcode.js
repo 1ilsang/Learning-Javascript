a3a38f363576d0b1ff15646a170e6b5eaddd04ad
sangchul <1ilsang@naver.com>
2018-03-08 23:09:23 +0900

    delete wrong string

--- mytestcode.js
+++ mytestcode.js
@@ -2,5 +2,3 @@ const words = ["1ilsang", "blog", "me", "sangchul", "catholic", "cuk"];
 const longWords = words.reduce((a, w) => w.length > 4 ?
     a + " " + w : a, "").trim();
 console.log(longWords);
-
-1 ilsang sangchul catholic

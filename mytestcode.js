43030fda2fa59e04558cf5dbf63b58b9e4f266f8
unknown <sangclee@smilegate.com>
2018-03-13 15:17:38 +0900

    mycode

--- mytestcode.js
+++ mytestcode.js
@@ -1,4 +1,1 @@
-const words = ["1ilsang", "blog", "me", "sangchul", "catholic", "cuk"];
-const longWords = words.reduce((a, w) => w.length > 4 ?
-    a + " " + w : a, "").trim();
-console.log(longWords);
+//1ilsang
